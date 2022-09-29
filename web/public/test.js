8ec6fa7ab236229782a056f627f31958e553a9ef
ddld93 <umar.jere@gmail.com>
2022-09-29 17:42:35 +0100

    fressh commit

--- web/public/test.js
+++
@@ -1,9 +0,0 @@
-function oddNumbers(l, r) {
-    let arr = []
-for (let index = 0; index < r; index++) {
-    if(index%2!=0) arr.push(index)
-}
-console.log(arr)
-}
-
-oddNumbers(1,10)
--- web/src/config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    userEndPoint:"http://user:3000",
-    formEndPoint:"http://form:3000"
-}
