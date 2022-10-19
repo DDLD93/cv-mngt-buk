a11b7689e212e0bb87db5dd37ee96925b8cfd574
ddld93 <umar.jere@gmail.com>
2022-10-19 23:04:03 +0100

    commit

---
+++ web/src/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCs-XTvd1nc__GAWwra8xF52Aa5WVjD1lg",
+  authDomain: "buk-cv.firebaseapp.com",
+  projectId: "buk-cv",
+  storageBucket: "buk-cv.appspot.com",
+  messagingSenderId: "23218796180",
+  appId: "1:23218796180:web:1deddbf4c934849e072681"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
