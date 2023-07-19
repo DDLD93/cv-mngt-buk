9a8ccd75be5e330472215ca9b2fa9de665cdef23
muhammadtanko <teekay014@gmail.com>
2023-07-19 14:21:35 +0100

    minor fixes

--- web/src/config.js
+++ web/src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    // userEndPoint:"https://users.agiletest.ng",
-    // formEndPoint:"https://forms.agiletest.ng"
-    userEndPoint:"http://localhost:4000",
-    formEndPoint:"http://localhost:4000"
+    userEndPoint:"https://users.agiletest.ng",
+    formEndPoint:"https://users.agiletest.ng"
+    // userEndPoint:"http://localhost:4000",
+    // formEndPoint:"http://localhost:4000"
 }
